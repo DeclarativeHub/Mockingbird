aaa8f0aaf38d7e0063bc3379cae078d5119825c2
Srdan Rasic <srdan.rasic@gmail.com>
2020-06-13 19:09:23 +0200

    Animation

--- Sources/Types/Animation.swift
+++ Sources/Types/Animation.swift
@@ -20,7 +20,89 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-public enum Animation: Equatable {
+import UIKit
 
-    case `default`
+public struct Animation {
+
+    public enum Repetition {
+        case none
+        case count(Int, autoreverses: Bool)
+        case forever(autoreverses: Bool)
+    }
+
+    public enum Kind {
+        case linear(easeIn: Bool, easeOut: Bool)
+        case spring(response: Double, dampingFraction: Double, blendDuration: Double)
+    }
+
+    public var kind: Kind = .linear(easeIn: false, easeOut: false)
+    public var delay: Double = 0
+    public var speed: Double = 1
+    public var duration: Double = 0.25
+    public var repetition: Repetition = .none
+
+    public static let `default`: Animation = Animation()
+
+    public static func spring(response: Double = 0.55, dampingFraction: Double = 0.825, blendDuration: Double = 0) -> Animation {
+        Animation(kind: .spring(response: response, dampingFraction: dampingFraction, blendDuration: blendDuration))
+    }
+
+    public static func easeInOut(duration: Double) -> Animation {
+        Animation(kind: .linear(easeIn: true, easeOut: true), duration: duration)
+    }
+
+    public static var easeInOut: Animation {
+        .easeInOut(duration: 0.25)
+    }
+
+    public static func easeIn(duration: Double) -> Animation {
+        Animation(kind: .linear(easeIn: true, easeOut: false), duration: duration)
+    }
+
+    public static var easeIn: Animation {
+        .easeIn(duration: 0.25)
+    }
+
+    public static func easeOut(duration: Double) -> Animation {
+        Animation(kind: .linear(easeIn: false, easeOut: true), duration: duration)
+    }
+
+    public static var easeOut: Animation {
+        .easeOut(duration: 0.25)
+    }
+
+    public static func linear(duration: Double) -> Animation {
+        Animation(kind: .linear(easeIn: false, easeOut: false), duration: duration)
+    }
+
+    public static var linear: Animation {
+        .linear(duration: 0.25)
+    }
+}
+
+extension Animation {
+
+    public func delay(_ delay: Double) -> Animation {
+        var copy = self
+        copy.delay = delay
+        return copy
+    }
+
+    public func speed(_ speed: Double) -> Animation {
+        var copy = self
+        copy.speed = speed
+        return copy
+    }
+
+    public func repeatCount(_ repeatCount: Int, autoreverses: Bool = true) -> Animation {
+        var copy = self
+        copy.repetition = .count(repeatCount, autoreverses: autoreverses)
+        return copy
+    }
+
+    public func repeatForever(autoreverses: Bool = true) -> Animation {
+        var copy = self
+        copy.repetition = .forever(autoreverses: autoreverses)
+        return copy
+    }
 }
